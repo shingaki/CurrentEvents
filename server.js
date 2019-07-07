ca992887c569a1b403aeeabc83a60492f12f493d
Tamami <tamami_polk@hotmail.com>
2019-07-07 16:14:56 -0500

    initial checking

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+ongoose.connect("mongodb://localhost/currentEvents", { useNewUrlParser: true });
+
+// var MONGODB_URI = process.env.MONGODB_URI
